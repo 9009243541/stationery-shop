d643b121dd0aacb10897ce5490d45aa42d154797
samyak jain <142584719+9009243541@users.noreply.github.com>
2025-08-14 16:28:35 +0530

    package

--- vite.config.js
+++ vite.config.js
@@ -5,36 +5,3 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
-
-// import { defineConfig } from 'vite'
-// import tailwindcss from '@tailwindcss/vite'
-
-// export default defineConfig({
-//   plugins: [tailwindcss()],
-//   server: {
-//     host: 'localhost', // or '0.0.0.0' if you're accessing from mobile or another device
-//     port: 5173,
-//     hmr: {
-//       protocol: 'ws',
-//       host: 'localhost',
-//       port: 5173,
-//     },
-//   },
-// })
-
-
-
-
-// import { defineConfig } from "vite";
-// import tailwindcss from "@tailwindcss/vite";
-
-// export default defineConfig({
-//   plugins: [tailwindcss()],
-//   server: {
-//     host: "localhost",
-//     port: 5173,
-//     hmr: {
-//       overlay: false, 
-//     },
-//   },
-// });
